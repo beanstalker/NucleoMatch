5468a84d4eed3d8214a05e8a4d51a089d5052f9c
John Hervey <johnhervey@Johns-MacBook-Pro.local>
2012-08-14 17:49:34 +1200

    added query pattern generator

--- NucleoMatch/main.m
+++ NucleoMatch/main.m
@@ -10,12 +10,19 @@
 #import <Foundation/Foundation.h>
 #import "SequenceLibrary.h"
 #import "Sequence.h"
+#import <stdio.h>
+#import <stdlib.h>
+#import <math.h>
 
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
         
+        
+        //Get an input file from a filepath given as an argument.
+        //Process the input file into individual sequences and store in a
+        // SequenceLibrary object.
         SequenceLibrary *library = [[SequenceLibrary alloc] init];
         Sequence *seq;
         NSProcessInfo *process = [NSProcessInfo processInfo];
@@ -58,6 +65,43 @@ int main(int argc, const char * argv[])
             [library addSequence:seq];
         }
         [library print];
+        
+        
+        //Calculate all k-mer query patterns. These will be the search terms
+        // when searching the sequences from the input file for frequent
+        // patterns.
+        NSArray *yAlphabet = [NSArray arrayWithObjects:@"A", @"C", @"G", @"U", nil];
+        NSArray *xAlphabet = [NSArray arrayWithObjects:@"A", @"C", @"G", @"U", @"?", nil];
+        NSMutableArray *queries = [[NSMutableArray alloc] init];
+        int i, v;
+        int k = 4;
+        int count = 0;
+        int numberOfCases = 16 * pow(5 , (k - 2));
+        
+        for (int n = 0; n < numberOfCases; n++) {
+            i = n;
+            v = i % 4;
+            i = i / 4;
+            count++;
+            NSMutableString *query = [[NSMutableString alloc] initWithString:[yAlphabet objectAtIndex:v]];
+            for (int m = 1; m < (k - 1); m++) {
+                v = i % 5;
+                i = i / 5;
+                [query appendString:[xAlphabet objectAtIndex:v]];
+            }
+            [query appendString:[yAlphabet objectAtIndex:i]];
+            [queries addObject:query];
+        }
+        NSLog(@"No. queries looped: %i", count);
+        
+        //print the array to confirm
+        int count1 = 0;
+        for (NSMutableString *str in queries) {
+            fprintf(stderr, "%s\n", [str cStringUsingEncoding:NSUTF8StringEncoding]);
+            count1++;
+        }
+        NSLog(@"No. queries printed: %i", count1);
+        NSLog(@"Counts match? : %@", (count == count1 ? @"YES" : @"NO"));
     }
     return 0;
 }
